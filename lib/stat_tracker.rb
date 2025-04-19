4a3bba78dae7932687788f512e284f2819c661e6
Austin Vanloon <wic404learn@gmail.com>
2025-04-19 17:00:43 -0600

    Initial commit, created stattracker class and test

---
+++ lib/stat_tracker.rb
@@ -0,0 +1,11 @@
+require 'csv'
+
+class StatTracker
+    def initialize(stat_tracker)
+        @stat_tracker = stat_tracker
+    end
+
+    def self.from_csv(locations)
+        headers 
+    end
+end
---
+++ spec/stat_tracker_spec.rb
@@ -0,0 +1,6 @@
+require './spec/spec_helper'
+
+RSpec.describe StatTracker do
+
+    expect().to be_a()
+end
