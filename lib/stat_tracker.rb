0926c69c0db8c2823cb051f111d8e3c279e9d8c8
Austin Vanloon <wic404learn@gmail.com>
2025-04-19 17:39:11 -0600

    Updated runner.rb and stat tracker

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,11 +1,24 @@
 require 'csv'
 
 class StatTracker
+    
     def initialize(stat_tracker)
         @stat_tracker = stat_tracker
     end
 
     def self.from_csv(locations)
-        headers 
+        games = CSV.read(locations[:games], headers: true, header_converters: :symbol)
+        teams = CSV.read(locations[:teams], headers: true, header_converters: :symbol)
+        game_teams = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol)
     end
 end
+
+
+#CSV.foreach('./data/animal_lovers.csv', headers: true, header_converters: :symbol) do |row| #out one level then in one level select file animal_lovers.csv
+# animal_lovers << AnimalLover.new
+# (
+#     row[:id].to_i, 
+#     row[:first_name], 
+#     row[:last_name], 
+#     row[:age].to_i
+#     )
--- runner.rb
+++ runner.rb
@@ -1,3 +1,5 @@
+require 'csv'
+require './lib/stat_tracker'
 
 game_path = './data/games.csv'
 team_path = './data/teams.csv'
@@ -12,4 +14,41 @@ locations = {
 @stat_tracker = StatTracker.from_csv(locations)
 
 require 'pry'; binding.pry
-require './lib/stat_tracker'
+
+# CSV.foreach(locations, headers: true, header_converters: :symbol) do |row|
+# locations = StatTracker.new(
+#     row[:game_id].to_i,
+#     row[:season],
+#     row[:type],
+#     row[:date_time],
+#     row[:away_team_id].to_i,
+#     row[:home_team_id].to_i,
+#     row[:away_goals].to_i,
+#     row[:home_goals].to_i,
+#     row[:venue],
+#     row[:venue_link],
+
+#     row[:team_id].to_i,
+#     row[:franchise_id].to_i,
+#     row[:team_name],
+#     row[:abbreviation],
+#     row[:stadium],
+#     row[:link],
+
+#     row[:game_id].to_i,
+#     row[:team_id].to_i,
+#     row[:hoa],
+#     row[:result],
+#     row[:settled_in],
+#     row[:head_coach],
+#     row[:goals].to_i,
+#     row[:shots].to_i,
+#     row[:tackles].to_i,
+#     row[:pim].to_i,
+#     row[:power_play_opportunities].to_i,
+#     row[:power_play_goals].to_i,
+#     row[:face_off_win_percentage].to_f,
+#     row[:giveaways].to_i,
+#     row[:takeaways].to_i
+#     )
+# end
