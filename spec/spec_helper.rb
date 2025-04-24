ef6a3268f9d526b0aa1a8fd24c09fbfb74ed63fe
Nick Rathbone <nrathbone@joinhandshake.com>
2025-04-23 23:46:54 -0600

    Refactor fixtures to allow for a wider dataset needed for game, league, and season stats; refactor spec_helper file to remove factories now that we're using fixtures instead

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,10 @@ require 'simplecov'
 SimpleCov.start do
   add_filter "/spec/"
 end
+
 require 'pry'
 require 'rspec'
 require_relative '../lib/stat_tracker'
 require_relative './game_spec'
 require_relative './team_spec'
 require_relative './game_team_spec'
-# require_relative './game_factory'
-# require_relative './team_factory'
-# require_relative './game_team_factory'
